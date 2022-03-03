5f7422be4e2b4c45a648f9d369bad96de60f84fb
brycen <brycenbb1996@gmail.com>
2022-03-03 19:41:02 +0000

    small updates, prevents leading zeros and storing first number properly

--- js-file.js
+++ js-file.js
@@ -13,7 +13,7 @@ function multiply(num1, num2) {
 }
 
 function divide(num1, num2) {
-    num1/num2;
+    return num1/num2;
 }
 
 function operate(operator, num1, num2){
@@ -34,46 +34,58 @@ function operate(operator, num1, num2){
     }
 }
 var currentDisplay = ""
-var firstNum = 0;
-var secondNum = 0;
+var firstNum = null;
+var secondNum = null;
+var lastPress = "";
 var lastDisplay = "";
-
-
+var operator;
 
 let display = document.querySelector('.text');
 
 //Handling button clicks
 const buttons = document.querySelectorAll('button');
-console.log(buttons);
 
-//Tried making a for loop instead of for each loop
-// for(i = 0; i < buttons.length;i++){
-
-//     //Checking if decimal button was hit
-//     if(buttons[i].classList.contains('decimal')){
-//         if(display.textContent.search(".") != -1){
-//             continue;
-//         }
-//         currentDisplay = currentDisplay + ".";
-
-
-//     }
-// }
-
-
+// Im going about this the wrong way, the order is
+// pick first number, then pick operator, then pick 2nd number
 
 buttons.forEach(button => {
     button.addEventListener('click', function() {
-        //Operator buttons
+        
+        //Decimal button
         if(button.classList.contains('decimal')){
             if(display.textContent.search(".") != -1) {
                 return;
             }
             currentDisplay = currentDisplay + "."
         }
+
+
+        //Operator buttons
+        if(button.classList.contains('symbol')) {
+            switch(button.textContent){
+                case ("\u00F7"):
+                    operator = "divide";
+                case ("\u00D7"):
+                    operator = "multiply";
+                case ("-"):
+                    operator = "subtract";
+                case ("+"):
+                    operator = "add";
+                case ("="):
+                    operator = "equals"
+            }
+
+            //Not resetting number if multiple symbols are hit in a row
+            if(firstNum === null){
+                firstNum = Number(display.textContent);
+            }
+            //resetting display for next number input
+            currentDisplay = "";
+            console.log(firstNum);
+            return;
+        }
         
-        
-        
+        //Clear button functionality 
         if(this.textContent === "AC"){
             display.textContent = "";
             currentDisplay = "";
@@ -95,10 +107,16 @@ buttons.forEach(button => {
             else {//percentage change stuff}
             }
         }
+        //Else is just a normal number
         else{
             lastDisplay = currentDisplay;
             currentDisplay = currentDisplay + this.textContent;
+            //Prevents leading zeros
+            if((currentDisplay.charAt(0) === "0") && (currentDisplay.length > 1)){
+                currentDisplay = currentDisplay.slice(1);
+            }
             display.textContent = currentDisplay;
+            
         }
         // console.log('clicked');
     })
